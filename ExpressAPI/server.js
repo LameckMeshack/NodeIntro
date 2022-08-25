301624476895c5e570a83b929635670bb8116fc6
RhinolamerSonwar <rhinolamer@gmail.com>
2022-08-25 23:56:15 +0300

    routes

--- ExpressAPI/server.js
+++ ExpressAPI/server.js
@@ -8,6 +8,12 @@ const hostname = "127.0.0.1";
 app.get("/", (req, res) => {
   res.send("Hello Sonwar the express server is up and running");
 });
+app.get("/api/interns", (req, res) => {
+  res.send("This is the intern api");
+});
+app.get("/api/interns/:id", (req, res) => {
+  res.send("This is the intern api with id");
+});
 
 app.listen(port, hostname, () => {
   console.log(`Your Server is running at http://${hostname}:${port}/`);
